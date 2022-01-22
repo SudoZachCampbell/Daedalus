4f95422863c53f152a1c78e17ff4ad61db533064
Zach Campbell <zcampbell@highroads.com>
2022-01-22 13:46:42 +0000

    Added flashlight script

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject[] spotLights;
+    private List<Light> lightColours;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(var spot in spotLights)
+        {
+            lightColours.Add(spot.GetComponent<Light>());
+        }
     }
 
     // Update is called once per frame
