535ef2d7842fe4497355a2df15c0fb272acd51fa
Zach Campbell <zcampbell@highroads.com>
2022-01-22 16:27:27 +0000

    Toggling Flashlight, changed FoV

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -6,19 +6,34 @@ public class Flashlight : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] spotLights;
-    private List<Light> lightColours;
+    private Dictionary<LightType, List<Color>> colours = new Dictionary<LightType, List<Color>>() {
+        {   LightType.Standard, new List<Color>() { new Color(0.6f, 0.62f, 0.3f), new Color(1.0f, 0.72f, 0.51f), new Color(0.67f, 0.36f, 0.16f) } },
+        {   LightType.UV, new List<Color>() { new Color(0.58f, 0f, 1.0f), new Color(0.78f, 0f, 1.0f), new Color(0.06f, 0f, 1.0f) } }
+    };
+    private List<Light> spotLightLights = new List<Light>();
     // Start is called before the first frame update
     void Start()
     {
-        foreach(var spot in spotLights)
-        {
-            lightColours.Add(spot.GetComponent<Light>());
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void SetLightColours(LightType type)
+    {
+        var selectedColours = colours[type];
+        for (int i = 0; i < spotLights.Length; i++)
+        {
+            spotLights[i].GetComponent<Light>().color = selectedColours[i];
+        }
+    }
+
+    public enum LightType
+    {
+        Standard,
+        UV
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+    [SerializeField]
+    private GameObject flashlightObj;
+    private Flashlight flashlight;
+    private bool clueMode = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateSingleton();
+        flashlight = flashlightObj.GetComponent<Flashlight>();
+    }
+
+    private void CreateSingleton()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("ToggleLight"))
+        {
+            clueMode = !clueMode;
+            flashlight.SetLightColours(clueMode ? Flashlight.LightType.UV : Flashlight.LightType.Standard);
+        }
     }
 }
