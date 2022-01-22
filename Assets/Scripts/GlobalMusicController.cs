c98af6b3e7e4590739f5bccde1622dc8dced903b
Robert Kordula <bobbykordula@gmail.com>
2022-01-22 22:20:17 +0000

    Added GlobalMusic prefab Global music will play background music by default When triggered global music will play chase music When triggered again will switch back to background music Added MonsterSounds prefab Will be the container for all monster sounds

--- Assets/Scripts/GlobalMusicController.cs
+++ Assets/Scripts/GlobalMusicController.cs
@@ -4,37 +4,36 @@ using UnityEngine;
 
 public class GlobalMusicController : MonoBehaviour
 {
-    AudioSource myAudioSource;
-    float countDown;
-    public bool beingChased;
-    bool ready = false;
-    bool playing = false;
+    public bool beingChased = false;
+    bool stateChange = false;
+    public AudioClip backgroundMusic;
+    public AudioClip chaseMusic;
+
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        myAudioSource = GetComponent<AudioSource>();
-        countDown = Random.Range(5.0f, 10.0f);
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = backgroundMusic;
+        audioSource.Play(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (countDown > 0) {
-            countDown -= Time.deltaTime;
+        if (!stateChange && beingChased) 
+        {  
+            stateChange = true;
+            audioSource.Stop();
+            audioSource.clip = chaseMusic;
+            audioSource.Play();
         }
-        if (countDown < 0) {
-            ready = true;
-            playNewAmbient();
-            countDown = Random.Range(5.0f, 10.0f);
-        }
-        double b = System.Math.Round(countDown, 2);
-        Debug.Log(b.ToString());
-    }
-
-    void playNewAmbient() {
-        if (ready && !playing) {
-            myAudioSource.Play();
-            playing = true;
+        if (stateChange && !beingChased)
+        {
+            stateChange = false;
+            audioSource.Stop();
+            audioSource.clip = backgroundMusic;
+            audioSource.Play();
         }
     }
 }
