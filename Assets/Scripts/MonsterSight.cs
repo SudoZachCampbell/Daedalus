3cd702244a862cf248e351e9001d890e3dd1bc8d
Robert Kordula <bobbykordula@gmail.com>
2022-01-23 01:11:08 +0000

    Added NavMesh to map Added MonsterSight script to trigger when the player collides with its detection cube Added MonsterMovement to navigate to the player when triggered

---
+++ Assets/Scripts/MonsterSight.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSight : MonoBehaviour
+{
+    const int MONSTER_SLEEP = 5;
+    public GameObject monster;
+    private float reset = MONSTER_SLEEP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        reset -= Time.deltaTime;
+        if (reset < 0)
+        {
+            monster.GetComponent<MonsterMovement>().enabled = false;
+        }            
+    }
+
+    void OnTriggerEnter(Collider collision) 
+    {
+        Debug.Log("COLLIDED");
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("COLLIDED");
+            monster.GetComponent<MonsterMovement>().enabled = true;
+            reset = MONSTER_SLEEP;
+        }
+    }
+}
