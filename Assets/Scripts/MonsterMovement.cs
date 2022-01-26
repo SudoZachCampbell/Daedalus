a032409c7c8d59511d855e9046340c60b1deafeb
SudoZachCampbell <zcam1995@gmail.com>
2022-01-26 19:17:48 +0000

    Updated nav mesh, spider AI

--- Assets/Scripts/MonsterMovement.cs
+++ Assets/Scripts/MonsterMovement.cs
@@ -1,50 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 public class MonsterMovement : MonoBehaviour
 {
     public GameObject player;
     private NavMeshAgent navmesh;
-    public Transform[] points;
-    int destPoint = -1;
+    public GameObject[] pointsArray;
+    private CapsuleCollider capCollider;
+    private LinkedListNode<GameObject> currentPoint;
+    private LinkedList<GameObject> points;
 
     public bool foundPlayer = false;
     // Use this for initialization
     void Start()
     {
         navmesh = GetComponent<NavMeshAgent>();
-        GotoNextPoint();
+        capCollider = GetComponent<CapsuleCollider>();
+        points = new LinkedList<GameObject>(pointsArray);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!foundPlayer) {
+        if (!foundPlayer)
+        {
             // Choose the next destination point when the agent gets
             // close to the current one.
-            if (!navmesh.pathPending && navmesh.remainingDistance < 0.5f)
-                GotoNextPoint();
+            if (!navmesh.pathPending && navmesh.remainingDistance < capCollider.radius * 3)
+            {
+                currentPoint = currentPoint?.Next ?? points.First;
+                Debug.Log($"Current Point: {currentPoint.Value.name}");
+            }
             // Set the agent to go to the currently selected destination.
-            navmesh.destination = points[destPoint].position;
+            navmesh.destination = currentPoint.Value.transform.position;
         }
-        if (foundPlayer) {
+        if (foundPlayer)
+        {
             navmesh.destination = player.transform.position;
         }
-    }
-    
-    void GotoNextPoint()
-    {
-        // Returns if no points have been set up
-        if (points.Length == 0)
-            return;
 
-        // Choose the next point in the array as the destination,
-        // cycling to the start if necessary.
-        destPoint = (destPoint + 1);
-        if (destPoint >= points.Length) {
-            destPoint = 0;
-        }
-        Debug.Log(destPoint);
     }
+
 }
